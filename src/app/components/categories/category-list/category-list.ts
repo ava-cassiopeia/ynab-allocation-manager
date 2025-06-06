8ff7e2564990c09810ab2602cf71b0b205051694
Ava Mattie <6314286+ava-cassiopeia@users.noreply.github.com>
2025-06-05 22:01:43 -0600

    Render currencies a bit better

--- src/app/components/categories/category-list/category-list.ts
+++ src/app/components/categories/category-list/category-list.ts
@@ -1,11 +1,13 @@
 import {Component, inject} from '@angular/core';
 
+import {Currency} from '../../common/currency/currency';
 import {YnabStorage} from '../../../../lib/ynab/ynab_storage';
 
 @Component({
   selector: 'ya-category-list',
   templateUrl: './category-list.html',
-  styleUrl: './category-list.scss'
+  styleUrl: './category-list.scss',
+  imports: [Currency],
 })
 export class CategoryList {
   protected readonly ynabStorage = inject(YnabStorage);
---
+++ src/app/components/common/currency/currency.ts
@@ -0,0 +1,20 @@
+import {Component, input, computed} from '@angular/core';
+
+/**
+ * Renders milliunit currency values as USD values.
+ */
+@Component({
+  selector: 'ya-currency',
+  templateUrl: './currency.html',
+  styleUrl: './currency.scss'
+})
+export class Currency {
+  readonly milliunits = input.required<number>();
+
+  protected readonly value = computed<string>(() => {
+    if (this.milliunits() === 0) return '-';
+
+    const currencyAmount = Math.round(this.milliunits() / 10.0) / 100.0;
+    return `$${currencyAmount}`;
+  });
+}
