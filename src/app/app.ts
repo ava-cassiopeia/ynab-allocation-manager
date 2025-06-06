e1eb1b6746a2ef0d666e6950d7b506de149b017e
Ava Mattie <6314286+ava-cassiopeia@users.noreply.github.com>
2025-06-06 16:25:49 -0600

    Setup the basics of the Firebase integration

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 
 import {BudgetSelector} from './components/budgets/budget-selector/budget-selector';
 import {CategoryList} from './components/categories/category-list/category-list';
 import {YnabStorage} from '../lib/ynab/ynab_storage';
 import {YnabTokenForm} from './components/auth/ynab-token-form/ynab-token-form';
+import {FirestoreStorage} from '../lib/firestore/firestore_storage';
 
 @Component({
   selector: 'app-root',
@@ -17,6 +18,12 @@ import {YnabTokenForm} from './components/auth/ynab-token-form/ynab-token-form';
     YnabTokenForm,
   ],
 })
-export class App {
+export class App implements OnInit {
   protected readonly ynabStorage = inject(YnabStorage);
+
+  private readonly firestoreStorage = inject(FirestoreStorage);
+
+  ngOnInit() {
+    this.firestoreStorage.signInUser();
+  }
 }
--- src/app/components/categories/category-list/category-list.ts
+++ src/app/components/categories/category-list/category-list.ts
@@ -1,7 +1,10 @@
 import {Component, inject} from '@angular/core';
+import {Category} from 'ynab';
 
 import {Currency} from '../../common/currency/currency';
 import {YnabStorage} from '../../../../lib/ynab/ynab_storage';
+import {FirestoreStorage} from '../../../../lib/firestore/firestore_storage';
+import {Allocation} from '../../../../lib/models/allocation';
 
 @Component({
   selector: 'ya-category-list',
@@ -11,4 +14,9 @@ import {YnabStorage} from '../../../../lib/ynab/ynab_storage';
 })
 export class CategoryList {
   protected readonly ynabStorage = inject(YnabStorage);
+  protected readonly firestoreStorage = inject(FirestoreStorage);
+
+  protected addAllocationFor(category: Category) {
+    this.firestoreStorage.addAllocation(new Allocation(category.id, 'fake_account_id'));
+  }
 }
