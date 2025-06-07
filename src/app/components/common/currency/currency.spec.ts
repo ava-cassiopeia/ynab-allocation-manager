74aa4afd1ff5f1717217016d001329701d200299
Ava Mattie <6314286+ava-cassiopeia@users.noreply.github.com>
2025-06-06 19:01:23 -0600

    Add more test coverage yay

--- src/app/components/common/currency/currency.spec.ts
+++ src/app/components/common/currency/currency.spec.ts
@@ -1,11 +1,9 @@
 import {provideZonelessChangeDetection} from '@angular/core';
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
 import {Currency} from './currency';
 
 describe('Currency', () => {
-  let component: Currency;
-  let fixture: ComponentFixture<Currency>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,14 +11,44 @@ describe('Currency', () => {
       providers: [provideZonelessChangeDetection()],
     })
       .compileComponents();
-
-    fixture = TestBed.createComponent(Currency);
-    fixture.componentRef.setInput('milliunits', 1000);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    const {component} = createComponent(1000);
     expect(component).toBeTruthy();
   });
+
+  it('should render an appropriate fractional amount', () => {
+    const {fixture} = createComponent(1234);
+    const renderedText = fixture.nativeElement.textContent.trim() as string;
+    expect(renderedText).toEqual('$1.23');
+  });
+
+  it('should show two digits for even fractionals', () => {
+    const {fixture} = createComponent(1200);
+    const renderedText = fixture.nativeElement.textContent.trim() as string;
+    expect(renderedText).toEqual('$1.20');
+  });
+
+  it('should render an appropriate amount for a small amount', () => {
+    const {fixture} = createComponent(12345);
+    const renderedText = fixture.nativeElement.textContent.trim() as string;
+    expect(renderedText).toEqual('$12.35');
+  });
+
+  it('should render an appropriate amount for a large amount', () => {
+    const {fixture} = createComponent(123450000);
+    const renderedText = fixture.nativeElement.textContent.trim() as string;
+    expect(renderedText).toEqual('$123450.00');
+  });
+
+  function createComponent(milliunits: number) {
+    const fixture = TestBed.createComponent(Currency);
+    fixture.componentRef.setInput('milliunits', milliunits);
+    fixture.detectChanges();
+    return {
+      fixture,
+      component: fixture.componentInstance,
+    };
+  }
 });
