646e4c8c3c35e2ad3733d500757f6a380e4ede9b
Ava <6314286+ava-cassiopeia@users.noreply.github.com>
2025-06-18 16:27:51 -0600

    Improve how the local environment sets itself up (#22)

--- src/lib/ynab/ynab_auth_manager.ts
+++ src/lib/ynab/ynab_auth_manager.ts
@@ -1,14 +1,30 @@
 import {Injectable} from "@angular/core";
 
-// TODO: Replace localhost dynamically with the actual host.
-// TODO: Add `state` param to foil XSRF attacks
-// Since this is a public URL we're redirecting the user to, this is safe
-// to check in, but the client secret used in oauth should be hidden.
-const OAUTH_TARGET = "https://app.ynab.com/oauth/authorize?client_id=thOD8wIVAOXHYQZ1SJx2GrnuSkyvxFZqg0zU2gx_xY0&redirect_uri=https%3A%2F%2Fyam.ynab.rocks%2Fauth&response_type=code&scope=";
-
 @Injectable({providedIn: 'root'})
 export class YnabAuthManager {
+  // TODO: Add `state` param to foil XSRF attacks
+  // Since this is a public URL we're redirecting the user to, this is safe
+  // to check in, but the client secret used in oauth should be hidden.
+  private readonly baseUrl = "https://app.ynab.com/oauth/authorize";
+  private readonly clientId = "thOD8wIVAOXHYQZ1SJx2GrnuSkyvxFZqg0zU2gx_xY0";
+  private readonly responseType = "code";
+  private readonly redirectUri: string;
+
+  constructor() {
+    const isLocalhost = window.location.hostname === "localhost";
+    if (isLocalhost) {
+      this.redirectUri = "http://localhost:5002/auth";
+    } else {
+      this.redirectUri = "https://yam.ynab.rocks/auth";
+    }
+  }
+
   beginAuth() {
-    window.location.href = OAUTH_TARGET;
+    const params = new URLSearchParams();
+    params.set("client_id", this.clientId);
+    params.set("redirect_uri", this.redirectUri);
+    params.set("response_type", this.responseType);
+    params.set("scope", "");
+    window.location.href = `${this.baseUrl}?${params.toString()}`;
   }
 }
