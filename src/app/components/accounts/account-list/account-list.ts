214d4e768c238a58f8a5c45383cb361399a3444e
Ava <6314286+ava-cassiopeia@users.noreply.github.com>
2025-07-13 18:41:02 -0600

    Sort accounts by type (#71)
    * Sort accounts by type
    
    * Add account type sums
    
    * Fix tests

--- src/app/components/accounts/account-list/account-list.ts
+++ src/app/components/accounts/account-list/account-list.ts
@@ -1,14 +1,54 @@
-import {Component, inject} from '@angular/core';
+import {AccountType} from 'ynab';
+import {Component, inject, computed} from '@angular/core';
 
 import {AccountSummary} from '../account-summary/account-summary';
-import {AccountData} from '../../../../lib/accounts/account_data';
+import {AccountData, AccountAllocation} from '../../../../lib/accounts/account_data';
+import {Currency} from '../../common/currency/currency';
 
 @Component({
   selector: 'ya-account-list',
   templateUrl: './account-list.html',
   styleUrl: './account-list.scss',
-  imports: [AccountSummary],
+  imports: [AccountSummary, Currency],
 })
 export class AccountList {
-  protected readonly accountData = inject(AccountData);
+  /**
+   * All of the user's accounts, sorted by type for easier comprehension.
+   */
+  protected readonly accounts = computed<SortedAccounts>(() => {
+    const accounts = this.accountData.accounts();
+
+    const checkingAccounts: AccountAllocation[] = [];
+    const savingsAccounts: AccountAllocation[] = [];
+    for (const account of accounts) {
+      if (account.account.type === AccountType.Savings) {
+        savingsAccounts.push(account);
+      } else {
+        checkingAccounts.push(account);
+      }
+    }
+
+    return {checkingAccounts, savingsAccounts};
+  });
+
+  protected readonly balances = computed<Sums>(() => {
+    const accounts = this.accounts();
+
+    const checking = accounts.checkingAccounts.map((a) => a.account.cleared_balance).reduce((p, c) => p + c, 0);
+    const savings = accounts.savingsAccounts.map((a) => a.account.cleared_balance).reduce((p, c) => p + c, 0);
+
+    return {checking, savings};
+  });
+
+  private readonly accountData = inject(AccountData);
+}
+
+interface SortedAccounts {
+  readonly checkingAccounts: AccountAllocation[];
+  readonly savingsAccounts: AccountAllocation[];
+}
+
+interface Sums {
+  readonly checking: number;
+  readonly savings: number;
 }
