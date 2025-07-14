002d45d72eb8f8e373e8a0e2d349b28d62708878
Ava Mattie <6314286+ava-cassiopeia@users.noreply.github.com>
2025-07-13 18:29:19 -0600

    Sort accounts by type

--- src/app/components/accounts/account-list/account-list.ts
+++ src/app/components/accounts/account-list/account-list.ts
@@ -1,7 +1,8 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, computed} from '@angular/core';
 
 import {AccountSummary} from '../account-summary/account-summary';
-import {AccountData} from '../../../../lib/accounts/account_data';
+import {AccountData, AccountAllocation} from '../../../../lib/accounts/account_data';
+import {AccountType} from 'ynab';
 
 @Component({
   selector: 'ya-account-list',
@@ -10,5 +11,29 @@ import {AccountData} from '../../../../lib/accounts/account_data';
   imports: [AccountSummary],
 })
 export class AccountList {
-  protected readonly accountData = inject(AccountData);
+  /**
+   * All of the user's accounts, sorted by type for easier comprehension.
+   */
+  protected readonly accounts = computed<SortedAccounts>(() => {
+    const accounts = this.accountData.accounts();
+
+    const checkingAccounts: AccountAllocation[] = [];
+    const savingsAccounts: AccountAllocation[] = [];
+    for (const account of accounts) {
+      if (account.account.type === AccountType.Savings) {
+        savingsAccounts.push(account);
+      } else {
+        checkingAccounts.push(account);
+      }
+    }
+
+    return {checkingAccounts, savingsAccounts};
+  });
+
+  private readonly accountData = inject(AccountData);
+}
+
+interface SortedAccounts {
+  readonly checkingAccounts: AccountAllocation[];
+  readonly savingsAccounts: AccountAllocation[];
 }
