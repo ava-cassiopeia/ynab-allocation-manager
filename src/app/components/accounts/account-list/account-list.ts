1624213e5be2edbd04e6a10e244443e387eb797c
Ava Mattie <6314286+ava-cassiopeia@users.noreply.github.com>
2025-08-08 19:29:24 -0600

    Use new account sorting

--- src/app/components/accounts/account-list/account-list.ts
+++ src/app/components/accounts/account-list/account-list.ts
@@ -1,9 +1,10 @@
 import {AccountType} from 'ynab';
 import {Component, inject, computed} from '@angular/core';
 
-import {AccountSummary} from '../account-summary/account-summary';
 import {AccountData, AccountAllocation} from '../../../../lib/accounts/account_data';
+import {AccountSummary} from '../account-summary/account-summary';
 import {Currency} from '../../common/currency/currency';
+import {compareAccounts} from '../../../../lib/accounts/account_lists';
 
 @Component({
   selector: 'ya-account-list',
@@ -18,9 +19,8 @@ export class AccountList {
   protected readonly accounts = computed<SortedAccounts>(() => {
     const accounts = this.accountData.accounts();
 
-    // TODO: Add sorting here.
-    const checkingAccounts: AccountAllocation[] = [];
-    const savingsAccounts: AccountAllocation[] = [];
+    let checkingAccounts: AccountAllocation[] = [];
+    let savingsAccounts: AccountAllocation[] = [];
     for (const account of accounts) {
       if (account.account.type === AccountType.Savings) {
         savingsAccounts.push(account);
@@ -29,14 +29,21 @@ export class AccountList {
       }
     }
 
+    checkingAccounts = checkingAccounts.sort((a, b) => compareAccounts(a, b));
+    savingsAccounts = savingsAccounts.sort((a, b) => compareAccounts(a, b));
+
     return {checkingAccounts, savingsAccounts};
   });
 
   protected readonly balances = computed<Sums>(() => {
     const accounts = this.accounts();
 
-    const checking = accounts.checkingAccounts.map((a) => a.account.cleared_balance).reduce((p, c) => p + c, 0);
-    const savings = accounts.savingsAccounts.map((a) => a.account.cleared_balance).reduce((p, c) => p + c, 0);
+    const checking = accounts.checkingAccounts
+        .map((a) => a.account.cleared_balance)
+        .reduce((p, c) => p + c, 0);
+    const savings = accounts.savingsAccounts
+        .map((a) => a.account.cleared_balance)
+        .reduce((p, c) => p + c, 0);
 
     return {checking, savings};
   });
