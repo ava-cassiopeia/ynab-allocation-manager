011f7012b208e904cb0e0251e11caf7efe234039
Ava Mattie <6314286+ava-cassiopeia@users.noreply.github.com>
2026-03-14 15:35:19 -0600

    Create mobile-specific menu

---
+++ src/app/components/header/mobile-menu/clear-allocations-dialog/clear-allocations-dialog.ts
@@ -0,0 +1,20 @@
+import {Component, inject} from '@angular/core';
+import {MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle} from '@angular/material/dialog';
+
+import {FirestoreStorage} from '../../../../../lib/firestore/firestore_storage';
+
+@Component({
+  selector: 'ya-clear-allocations-dialog',
+  templateUrl: './clear-allocations-dialog.html',
+  styleUrl: './clear-allocations-dialog.scss',
+  imports: [MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose],
+})
+export class ClearAllocationsDialog {
+  private readonly firestoreStorage = inject(FirestoreStorage);
+  private readonly dialogRef = inject(MatDialogRef);
+
+  protected clear() {
+    void this.firestoreStorage.clearAllocationsForBudget();
+    this.dialogRef.close();
+  }
+}
